98e4f2e0f4a3b656f01a14696fc763c35b02d55a
EshaHassan2005 <sanjanahassan527@gmail.com>
2026-01-31 21:57:56 +0600

    Added user

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'purrito',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
